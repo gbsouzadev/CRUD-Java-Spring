5385ade51c40fc9aa7d1a86661996e73d795b8ae
Gabriel Souza <gbsouza.dev@gmail.com>
2023-05-29 21:46:02 -0300

    create ClientDTO and repository, service, controller layers

--- crud/src/main/java/com/example/crud/dto/ClientDTO.java
+++ crud/src/main/java/com/example/crud/dto/ClientDTO.java
@@ -1,33 +1,35 @@
-package com.example.crud.entities;
+package com.example.crud.dto;
 
-import jakarta.persistence.*;
+import com.example.crud.entities.Client;
 
 import java.time.Instant;
-import java.util.Objects;
 
-@Entity
-@Table(name="tb_client")
-public class Client {
+public class ClientDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
     private Double income;
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
     private Integer children;
 
-    public Client() {
+    public ClientDTO() {
     }
 
-    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+    public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
         this.name = name;
         this.cpf = cpf;
         this.income = income;
         this.birthDate = birthDate;
         this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.income = entity.getIncome();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
     }
 
     public Long getId() {
@@ -76,18 +78,5 @@ public class Client {
 
     public void setChildren(Integer children) {
         this.children = children;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return id.equals(client.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
