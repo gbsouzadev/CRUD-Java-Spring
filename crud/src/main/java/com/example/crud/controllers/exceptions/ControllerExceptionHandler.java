defd4db5114c5036f9b463107f9379bf2c144e6d
Gabriel Souza <gbsouza.dev@gmail.com>
2023-05-30 10:13:50 -0300

    Standarderror class, ControllerExceptionHandler, Exception treatment for Clients findById method

---
+++ crud/src/main/java/com/example/crud/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.crud.controllers.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @Autowired
+    StandardError err;
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFoundException(EntityNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Entity not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
+}
--- crud/src/main/java/com/example/crud/services/ClientService.java
+++ crud/src/main/java/com/example/crud/services/ClientService.java
@@ -3,6 +3,7 @@ package com.example.crud.services;
 import com.example.crud.dto.ClientDTO;
 import com.example.crud.entities.Client;
 import com.example.crud.repositories.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +19,8 @@ public class ClientService {
     @Transactional
     public ClientDTO findById(Long id) {
         Optional<Client> obj = repository.findById(id);
-        Client entity = obj.get();
+        // Client entity = obj.get();
+        Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Can't find a Client corresponding the informed id: " + id));
         return new ClientDTO(entity);
     }
 
