7d04127ac33ae29217ea48358a228d7d7d9c09a7
Gabriel Souza <gbsouza.dev@gmail.com>
2023-06-01 10:24:02 -0300

    refactor pageable, update postman collection

--- crud/src/main/java/com/example/crud/controllers/ClientController.java
+++ crud/src/main/java/com/example/crud/controllers/ClientController.java
@@ -5,6 +5,7 @@ import com.example.crud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,14 +21,8 @@ public class ClientController {
     ClientService service;
 
     @GetMapping
-    public ResponseEntity<Page<ClientDTO>> findAllPagable(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") Sort.Direction direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ){
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage,direction, orderBy);
-        Page<ClientDTO> list = service.findAllPagable(pageRequest);
+    public ResponseEntity<Page<ClientDTO>> findAllPagable(Pageable pageable){
+        Page<ClientDTO> list = service.findAllPagable(pageable);
         return ResponseEntity.ok().body(list);
     }
 
--- crud/src/main/java/com/example/crud/services/ClientService.java
+++ crud/src/main/java/com/example/crud/services/ClientService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +21,8 @@ public class ClientService {
     ClientRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<ClientDTO> findAllPagable(PageRequest pageRequest) {
-        Page<Client> list = repository.findAll(pageRequest);
+    public Page<ClientDTO> findAllPagable(Pageable pageable) {
+        Page<Client> list = repository.findAll(pageable);
         return list.map(client -> new ClientDTO(client));
     }
 
